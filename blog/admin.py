a4588be3aac68f57253c7313fb9a3f5310626d1a
Amanda Anumba <Amanda@Amandas-MacBook-Pro.local>
2016-07-08 22:37:23 -0400

    test changes for heroku

--- blog/admin.py
+++ blog/admin.py
@@ -1,22 +1,3 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.contrib.auth.models import User
 
-from .models import *
-
-class MemberInline(admin.StackedInline):
-    model = Member
-    can_delete = False
-    verbose_name_plural = 'member'
-
-# Define a new User admin
-class UserAdmin(BaseUserAdmin):
-    inlines = (MemberInline, )
-
-# Re-register UserAdmin
-admin.site.unregister(User)
-admin.site.register(User, UserAdmin)
-admin.site.register(Article)
-admin.site.register(Comment)
-admin.site.register(Cycle)
-admin.site.register(Topic)
+# Register your models here.
--- blog/urls.py
+++ blog/urls.py
@@ -4,14 +4,14 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^home/$', views.home, name="home"),
-    url(r'^about/$', views.about, name="about"),
-    url(r'^contact/$', views.contact, name="contact"),
-    url(r'^terms/$', views.terms, name="terms"),
-    url(r'^privacy/$', views.privacy, name="privacy"),
-    url(r'^userlogin/$', views.userlogin, name="userlogin"),
+    # url(r'^home/$', views.home, name="home"),
+    # url(r'^about/$', views.about, name="about"),
+    # url(r'^contact/$', views.contact, name="contact"),
+    # url(r'^terms/$', views.terms, name="terms"),
+    # url(r'^privacy/$', views.privacy, name="privacy"),
+    # url(r'^userlogin/$', views.userlogin, name="userlogin"),
     url(r'^(?P<cat>\w+)/$', views.category, name="category"),
     url(r'^(?P<cat>\w+)/(?P<subcat>\w+)/$', views.subcategory, name="subcategory"),
-    url(r'^(?P<cat>\w+)/(?P<slug>[\w-]+)/$', views.read, {'subcat':'none'}, name="read"),
-    url(r'^(?P<cat>\w+)/(?P<subcat>\w+)/(?P<slug>[\w-]+)/$', views.read, name="read"),
+    # url(r'^(?P<cat>\w+)/(?P<slug>[\w-]+)/$', views.read, {'subcat':'none'}, name="read"),
+    # url(r'^(?P<cat>\w+)/(?P<subcat>\w+)/(?P<slug>[\w-]+)/$', views.read, name="read"),
 ]
